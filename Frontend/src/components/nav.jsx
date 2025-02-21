a3f6cc46a56a1d6a180e468e754992860d870514
Akorede Ethan <koredeajibola@gmail.com>
2025-02-21 15:20:55 +0100

    updated

---
+++ Frontend/src/components/nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div>
+      <section>
+        <h1 className="">cdPayQuick</h1>
+        <div>
+          <button>Home</button>
+          <button>Individual</button>
+          <button>Business</button>
+        </div>
+        <div>
+          <button>Get Started</button>
+        </div>
+      </section>
+    </div>
+  );
+}
