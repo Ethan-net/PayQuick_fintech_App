a3a0ddadc02bf6028c32bb1af6fc58f96a4f6b11
heedarah <164653650+heedarah@users.noreply.github.com>
2025-02-26 07:25:10 -0800

    Worked on the about section

---
+++ Frontend/src/components/AboutAndProducts/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function About() {
+  return (
+    <div class="about-container">
+      <div class="about-content">
+        <h2>About Us</h2>
+        <div class="about-right">
+          <h3>Founded in 2022</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequatduis enim velit mollit Exercitation.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
